f606ed0fece33f5d9dbd7b964157cd34148d46d4
DeividZavala <crusedmoss@gmail.com>
2017-06-10 13:32:35 -0500

    agregando fetch :beers:

--- noveno-sabado/peliculas/src/components/home/home.js
+++ noveno-sabado/peliculas/src/components/home/home.js
@@ -3,6 +3,22 @@ import CardItem from '../card/cardItem'
 
 class Home extends React.Component{
 
+    constructor(){
+        super();
+        this.state = {
+            items: []
+        }
+    }
+
+    componentWillMount(){
+        fetch('https://api.themoviedb.org/4/list/1?page=1&api_key=b5b664b4028c36029bbeaf55cc2b6fb9&language=es')
+            .then(function (response) {
+                return response.json();
+            }).then(function (data) {
+                console.log(data);
+            })
+    }
+
     render(){
         return(
             <div>
