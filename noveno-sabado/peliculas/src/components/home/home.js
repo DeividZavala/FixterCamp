c81f93a6ff534be5490bfca6e7f5759ef701157d
DeividZavala <crusedmoss@gmail.com>
2017-06-10 13:56:13 -0500

    dibujando cards :beers:

--- noveno-sabado/peliculas/src/components/home/home.js
+++ noveno-sabado/peliculas/src/components/home/home.js
@@ -1,5 +1,19 @@
 import React from 'react';
 import CardItem from '../card/cardItem'
+import {GridList} from 'material-ui';
+
+const styles = {
+  root: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'space-around',
+  },
+  gridList: {
+    display: 'flex',
+    flexWrap: 'nowrap',
+    overflowX: 'auto',
+  }
+};
 
 class Home extends React.Component{
 
@@ -11,20 +25,40 @@ class Home extends React.Component{
     }
 
     componentWillMount(){
+
+        let updateState = (data) => {
+            this.setState({
+                items: data.results
+            })
+            console.log("Actualizado papud", this.state.items);
+        }
+
         fetch('https://api.themoviedb.org/4/list/1?page=1&api_key=b5b664b4028c36029bbeaf55cc2b6fb9&language=es')
             .then(function (response) {
                 return response.json();
             }).then(function (data) {
-                console.log(data);
+                updateState(data)
             })
+
     }
 
     render(){
         return(
-            <div>
-                Hola
-                <CardItem/>
-            </div>
+            <section>
+                Pelis Mijow
+
+                <div style={styles.root}>
+                    <GridList
+                    cols={3}
+                    cellHeight={900}
+                    style={styles.gridList}>
+
+                    {this.state.items.map((peli)=> <CardItem key={peli.id}/>)}
+
+                    </GridList>
+                </div>
+                
+            </section>
         );
     }
 
