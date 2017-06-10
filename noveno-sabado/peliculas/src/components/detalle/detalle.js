571bc4a17178c209f1015963295b5ed786bd0bad
DeividZavala <crusedmoss@gmail.com>
2017-06-10 15:31:30 -0500

    Consumiendo datos de la peli :octocat:

--- noveno-sabado/peliculas/src/components/detalle/detalle.js
+++ noveno-sabado/peliculas/src/components/detalle/detalle.js
@@ -2,10 +2,34 @@ import React from 'react';
 
 class Detail extends React.Component{
 
-    
+    constructor(){
+        super();
+        this.state = {
+            peli: {}
+        }
+    }
+
+    componentWillMount(){
+
+        let updateState = (data) => {
+            this.setState({
+                peli: data
+            })
+            console.log("Actualizado papud", this.state.items);
+        }
+
+        fetch('https://api.themoviedb.org/3/movie/'+ this.props.match.params.id +'?api_key=b5b664b4028c36029bbeaf55cc2b6fb9&language=es')
+            .then(function (response) {
+                return response.json();
+            }).then(function (data) {
+                updateState(data)
+            })
+
+    }
+
     render(){
         return(
-            <div>Detalle de la peli {this.props.match.params.id}</div>
+            <div>Detalle de la peli {this.state.peli.title} con el id -- {this.props.match.params.id}</div>
         );
     }
 }
